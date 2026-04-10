8eca9463f52384813f64034d6ad389584f82caf9
cvst1106-ux <cvst1106@gmail.com>
2026-04-10 19:10:22 -0400

    actualizacion del dashboard

--- app.py
+++ app.py
@@ -9,13 +9,21 @@ Amazon = ["#D46A07", "#1D2B44", "#3A5285", "#AFAFB3"]
 # se configura como se ve la pestaña
 st.set_page_config(
     page_title = "Dashboard Amazon",
-    page_icon = "📊")
-# Se configura el titulo de la pagina
-st.title("Analisis de precios y descuentos de Amazon", text_alignment = "center")
-st.write("**Dashboard interactivo** del análisis del catálogo de Amazon," \
+    #logo amazon?
+    page_icon = "amazon-icon-seeklogo.png",
+    layout= "wide")
+
+# Se configura el titulo de la pagina y el logoo
+#main?
+col1,col2 = st.columns([0.2,0.8])
+with col1:
+    st.image("Amazon_dashboard__1_-removebg-preview.png", width=400)
+with col2:
+    st.title("Analisis de precios y descuentos de Amazon",text_alignment = "center" )
+st.write("#### **Dashboard interactivo** del análisis del catálogo de Amazon," \
 " acerca de la relación entre precios originales, descuentos y categorías.")
 st.markdown("---")
-# Se carga el archivo
+# Se carga el archivo con el cache
 @st.cache_data
 def cargar_datos():
     df = pd.read_csv("amazon_limpio.csv")
@@ -23,58 +31,84 @@ def cargar_datos():
 df = cargar_datos()
 # Se manipula la variable categoria para poder trabajar con su primer elemento de la lista
 df['subcategoria'] = df['categoria'].str.split('|').str[0]
+
+# se agrupan precios por categorias con cuartiles
+economico = df["precio_original"].quantile(0.25)
+lujo = df["precio_original"].quantile(0.75)
+def clasificar_producto(precio):
+    if precio <= economico:
+        return "Económico"
+    elif precio >= lujo:
+        return "Lujo"
+    else:
+        return "Estandar"
+df["categoria_de_precio"]  = df["precio_original"].apply(clasificar_producto)
+        
 # Se configura la barra lateral
 with st.sidebar:
- # Se coloca un filtrado por categoria
-    st.sidebar.header("filtro por categoria 🔎")
-    categoria = st.multiselect(
+# Se coloca un filtro por categoria
+    st.header("categoria por producto 🔎")
+    filtro1 = st.multiselect(
     "Selecciona las categorias:",
-    options=df["subcategoria"].unique())
+    options=df["subcategoria"].unique(),
+    default=df["subcategoria"].unique())
     st.markdown("---")
-# Se coloca un filtrado por precio y uno por descuento
+# Se agrega un filtro por categoria de precio
+    st.header("🏷️categoria por precio")
+    filtro2 =  st.multiselect("Selecciona las categorias:",
+    options=df["categoria_de_precio"].unique(),
+    default=df["categoria_de_precio"].unique())
+    st.markdown("***")
+# Se coloca un filtrado por precio y uno por descuento 
     precio_min = int(df["precio_original"].min())
     precio_max = int(df["precio_original"].max())
-    rango_precio = st.slider(
+    filtro3 = st.slider(
         "Rango de precio (₹):",
         min_value=precio_min,
         max_value=precio_max,
         value=(precio_min, precio_max),
         step=100,
         format="₹%d")
-    
-    precio_min = int(df["precio_descuento"].min())
-    precio_max = int(df["precio_descuento"].max())
-    rango_precio = st.slider(
+
+    descuento_min = int(df["porcentaje_descuento"].min())
+    descuento_max = int(df["porcentaje_descuento"].max())
+    filtro4 = st.slider(
         "porcentaje de descuento (%):",
         min_value=0,
         max_value=100,
         value=(0,100),
         format="%d%%")
     st.markdown("***")
-    
-    economico = df["precio_original"].quantile(0.25)
-    lujo = df["precio_original"].quantile(0.75)
-    def clasificar_producto(precio):
-        if precio <= economico:
-            return "Económico"
-        elif precio >= lujo:
-            return "Lujo"
-        else:
-            return "Estandar"
-    
-    df["categoria_de_precio"]  = df["precio_original"].apply(clasificar_producto)
-    precio_por_categoria = st.multiselect("Selecciona las categorias:",
-    options=df["categoria_de_precio"].unique())
-    
-# se añaden columnas con informacion relevante
-st.header("")
 
-col1, col2 = st.columns(2)
+
+# Se conecta los filtros con los graficos y columnas para que sea interactivo
+
+if filtro1:
+    f_categoria = df["subcategoria"].isin(filtro1)
+else:
+    f_categoria = True
+if filtro2:
+    f_cprecio = (df["categoria_de_precio"].isin(filtro2)) 
+else:
+    f_cprecio = True
+f_porcentaje =(df["porcentaje_descuento"]>= descuento_min) & (df["porcentaje_descuento"]<= descuento_max)
+f_precio = (df["precio_original"]>= precio_min) & (df["precio_original"]<= precio_max)
+
+df_filtrado = df[f_categoria & f_cprecio & f_porcentaje & f_precio]
+
+# se añaden columnas con informacion relevante
+st.header("Resumen del catalogo de Amazon")
+
+col1, col2, col3,col4 = st.columns(4)
 
 with col1:
-    st.metric("📦 Total de productos", f"{len(df):,}", border= True)
-
+    st.metric("📦 Total de productos", f"{len(df_filtrado):,}", border= True)
 with col2:
-    st.metric(" Categorías", df["subcategoria"].nunique(), border= True)
+    st.metric(" Categorías", df_filtrado["subcategoria"].nunique(), border= True)
+with col3:
+    st.metric("precio promedio",f'₹{df_filtrado["precio_original"].mean():,.0f}', border= True)
+with col4:
+    st.metric("descuento promedio", f'{df_filtrado["porcentaje_descuento"].mean().round(1)}%', border= True)
 
+st.dataframe(df.style.highlight_max(axis=0))
 
